e295720638eae4174fc14bec7947a59bc04858e9
Alexander Fitterling <info@sp33c.de>
2014-11-06 03:52:15 +0100

    take off famous

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,6 @@
 'use strict';
 
 angular.module('famousAngular')
-  .controller('MainCtrl', ['$log', '$scope', '$famous', '$resource', '$http', function ($log, $scope, $famous, $resource, $http) {
-    var Transitionable = $famous['famous/transitions/Transitionable'];
-    var Timer = $famous['famous/utilities/Timer'];
-
-    $scope.spinner = {
-      speed: 55
-    };
-    $scope.rotateY = new Transitionable(0);
-
-    //run function on every tick of the Famo.us engine
-    Timer.every(function(){
-      var adjustedSpeed = parseFloat($scope.spinner.speed) / 1200;
-      $scope.rotateY.set($scope.rotateY.get() + adjustedSpeed);
-    }, 1);
+  .controller('MainCtrl', ['$log', '$scope', '$resource', '$http', function ($log, $scope, $resource, $http) {
 
   }]);
