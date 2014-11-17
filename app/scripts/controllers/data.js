f17997a63d12343fdc60bf9fd2daa8354d9c6509
Alexander Fitterling <info@sp33c.de>
2014-11-17 08:30:02 +0100

    save items

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -3,14 +3,21 @@
 angular.module('famousAngular')
   .controller('DataCtrl', ['$log', '$scope', '$resource', '$http', function ($log, $scope, $resource, $http) {
     var self = this;
-    var Items = $resource($scope.conf.API_BASEURL + '/items');
+
+    var Items = $resource($scope.conf.API_BASEURL + '/items', {id: '@id'},
+      { update: { method: 'PATCH', headers: { 'Content-Type': 'application/json' } } });
+
     self.items = Items.query(function (data) {
       console.log(data);
     });
 
     $scope.addItem = function (item) {
       console.log(item);
-      return {};
+      Items.save(item, function(success){
+//        console.log(success);
+        self.items.push(success);
+        return {};
+      });
     };
 
   }]);
