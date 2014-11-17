13ace80248826aa87944b6b2897ad3f6ccc79eed
Alexander Fitterling <info@sp33c.de>
2014-11-17 08:04:46 +0100

    $resource items

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -2,5 +2,9 @@
 
 angular.module('famousAngular')
   .controller('DataCtrl', ['$log', '$scope', '$resource', '$http', function ($log, $scope, $resource, $http) {
-
+    var self = this;
+    var Items = $resource($scope.conf.API_BASEURL + '/items');
+    self.items = Items.query(function (data) {
+      console.log(data);
+    });
   }]);
