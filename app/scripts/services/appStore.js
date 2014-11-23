9125002ca339017ae55797ddf6458805486dd26b
Alexander Fitterling <info@sp33c.de>
2014-11-23 12:41:19 +0100

    appStore

--- app/scripts/services/appStore.js
+++ app/scripts/services/appStore.js
@@ -1,22 +1,36 @@
 'use strict';
 
 // build a AppStore with Promises rather than
-// AppStore['key'] = setItems;
+// e.g. AppStore['key'] = setItems; (!angular anti pattern)
 angular.module('famousAngular')
 
   .factory('AppStore', ['$rootScope', '$q', function ($rootScope, $q) {
 
     var appStore = $rootScope.$new();
 
+    // promises go here
     appStore.q = {};
+    // the deferred if called with null obj goes here
+    appStore.deferred = {};
 
     return {
+      // simple trick: q.resolve only on data !null so
+      // we can create promises beforehand to able to resolve on them at state switching
       set: function (key, data) {
-        var deferred = $q.defer();
-        appStore.q[key] = deferred.promise;
-        // simple trick: q.resolve only on !null so we can create promises beforehand to able to resolve on them at state switching
-        if (data) {
-          deferred.resolve(data);
+        var deferred;
+        // if data set null but called with key
+        if (!appStore.q.hasOwnProperty(key) && !data) {
+          // create the defer call
+          deferred = $q.defer();
+          // save it to pick it up when called with real data
+          appStore.deferred[key] = deferred;
+          // save the deferred promise
+          appStore.q[key] = deferred.promise;
+        } else {
+          deferred = appStore.deferred[key];
+          if (data) {
+            deferred.resolve(data);
+          }
         }
       },
       get: function (key) {
