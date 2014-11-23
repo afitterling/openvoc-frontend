954cd153b7aa09271b2a69c173dc3cce018adaf4
Alexander Fitterling <info@sp33c.de>
2014-11-23 16:05:54 +0100

    appStore

--- app/scripts/services/appStore.js
+++ app/scripts/services/appStore.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// build a AppStore with Promises rather than
-// e.g. AppStore['key'] = setItems; (!angular anti pattern)
+// build a AppStore with Promises
 angular.module('famousAngular')
 
   .factory('AppStore', ['$rootScope', '$q', function ($rootScope, $q) {
@@ -10,16 +9,16 @@ angular.module('famousAngular')
 
     // promises go here
     appStore.q = {};
-    // the deferred if called with null obj goes here
+    // the deferred if called with null obj goes here (pe-initialization, to keep identical promises if we not yet know the data)
     appStore.deferred = {};
 
     return {
-      // simple trick: q.resolve only on data !null so
-      // we can create promises beforehand to able to resolve on them at state switching
+      // we can create promises beforehand to be able to resolve on them at state switching
+      // set latter called with data, the identical promise gets returned
       set: function (key, data) {
         var deferred;
-        // if data set null but called with key
-        if (!appStore.q.hasOwnProperty(key) && !data) {
+        // if data set null but called with key (initialization)
+        if (!appStore.q.hasOwnProperty(key)) {
           // create the defer call
           deferred = $q.defer();
           // save it to pick it up when called with real data
@@ -27,10 +26,12 @@ angular.module('famousAngular')
           // save the deferred promise
           appStore.q[key] = deferred.promise;
         } else {
+          // the deferred object has been stored before
           deferred = appStore.deferred[key];
-          if (data) {
-            deferred.resolve(data);
-          }
+        }
+        // if data resolve deferred
+        if (data) {
+          deferred.resolve(data);
         }
       },
       get: function (key) {
