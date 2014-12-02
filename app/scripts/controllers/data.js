3072a920aa7d1f8b922a09da0cde9ac3075b88fc
Alexander Fitterling <info@sp33c.de>
2014-12-02 07:05:33 +0100

    cleanup

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -112,39 +112,11 @@ angular.module('famousAngular')
         });
       };
 
-      // update model
-      // we use a trick here:
-      // as the editable allows editable per attr of one obj/item we end up in having multiple change/update requests per obj or id
-      // as of this fact we update with timeouts/promises and cancel the pre chosen promises if other fields got edited as well
-      // as the parameter is always the whole model this won't matter
-      var cancelUpdate = {};
-      $scope.updateItem = function (item) {
-        $log.debug('cancelUpdate', cancelUpdate);
-        if (angular.isDefined(cancelUpdate[item.id])) {
-          if (cancelUpdate[item.id].status !== 1) { // 1 => resolved
-            cancelUpdate[item.id]();
-          }
-        }
-        cancelUpdate[item.id] = $timeout(function () {
-          $log.debug('update triggered', item);
-          $scope.success = null;
-          items.update(item, function (success) {
-            $scope.success = true;
-            delete cancelUpdate[item.id]; // delete the promise from list as we know it ran through
-            $log.debug('cancelUpdate', cancelUpdate);
-          }, function (error) {
-            $scope.success = false;
-            $log.debug('cancelUpdate', cancelUpdate);
-          });
-        }, 5000);
-        $log.debug('cancelUpdate', cancelUpdate);
       };
 
-      $scope.deleteItem = function (item) {
-        items.delete({id: item.id}, function (success) {
-          self.items.splice(self.items.indexOf(item), 1);
-          $log.debug('deleted item', item);
-        });
+      // dummy
+      $scope.dummy = function () {
+        //
       };
 
     }]);
