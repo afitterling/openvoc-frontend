fac716db09f2a8ee46e78517c62b80b5c70051c8
Alexander Fitterling <info@sp33c.de>
2014-12-09 14:33:49 +0100

    learning

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,12 +6,19 @@ angular.module('famousAngular')
       //
     }])
 
-  .controller('LearnCtrl', ['$log', '$scope', '$resource', '$http',
-    function ($log, $scope, $resource, $http) {
+  .controller('LearnCtrl', ['$log', '$scope', '$resource', '$http', '$rootScope',
+    function ($log, $scope, $resource, $http, $rootScope) {
       //
+
+
+      var AutoUnit = $resource($scope.conf.API_BASEURL + '/unit_items/auto_generate');
 
       $scope.start = function () {
         // fetch the custom unit with age param
+        console.log($scope.lang);
+        AutoUnit.query({language_id: $scope.lang.from.id, targetlang_id: $scope.lang.to.id}, function (data) {
+          console.log(data);
+        });
 
 //        $scope.learn = true;
 //        $scope.index = 0;
