584a0f21cde9784f0944666bbd0b72a192bae459
Alexander Fitterling <info@sp33c.de>
2014-12-10 08:31:47 +0100

    preliminary learn mode finished

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,17 +15,35 @@ angular.module('famousAngular')
 
         // fetch the custom unit
 
-        AutoUnit.query({shuffle: true, user_id: auth.profile.user_id, language_id: $scope.lang.from.id, targetlang_id: $scope.lang.to.id}, function (data) {
-          console.log(data);
+        AutoUnit.query({shuffle: true, user_id: auth.profile.user_id, language_id: $scope.lang.from.id, targetlang_id: $scope.lang.to.id}, function (success) {
+          $scope.unit_items = success;
         });
 
         $scope.unit = { inProgress: true, idx: 0 };
-
+        $scope.show = false;
       };
 
       $scope.stopUnit = function () {
 
         $scope.unit = { inProgress: false, idx: 0 };
+
+      };
+
+      $scope.next = function () {
+        console.log($scope.unit.idx);
+        $scope.unit.idx += 1;
+        $scope.show = false;
+      };
+
+      $scope.prev = function () {
+        console.log($scope.unit.idx);
+        $scope.unit.idx -= 1;
+        $scope.show = false;
+      };
+
+      $scope.showItem = function () {
+
+        $scope.show = !$scope.show;
 
       };
 
