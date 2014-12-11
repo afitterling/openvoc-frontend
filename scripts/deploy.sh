147c9f9ab9657d293e8a5aa66e972bdce23843cf
Alexander Fitterling <info@sp33c.de>
2014-12-11 17:41:37 +0100

    deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-gulp clean
-gulp build
-rsync -avr app/bower_components dist/
-./scripts/patch_baseUrl.sh 
+gulp clean && \
+gulp build && \
+rsync -avr app/bower_components dist/ && \
+./scripts/patch_baseUrl.sh && \
 ./scripts/push.sh
