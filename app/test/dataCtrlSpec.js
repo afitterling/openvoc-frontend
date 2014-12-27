62f4de25f50d3d9fa2f927ba7daff5ad74476b19
Alexander Fitterling <info@sp33c.de>
2014-12-27 10:39:01 +0100

    karma

---
+++ app/test/dataCtrlSpec.js
@@ -0,0 +1,18 @@
+describe('Controller: DataCtrl', function() {
+
+  // Instantiate a new version of my module before each test
+  beforeEach(module('famousAngular'));
+
+  var ctrl;
+
+  // Before each unit test, instantiate a new instance
+  // of the controller
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('DataCtrl');
+  }));
+
+  it('should have ....', function() {
+  });
+
+});
+
