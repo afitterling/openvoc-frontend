7dfabb20ad04f58c4dfb6793c1d63f8adeb01ad8
Alexander Fitterling <info@sp33c.de>
2015-01-10 19:26:50 +0100

    bugfix change lang

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -344,6 +344,7 @@ angular.module('famousAngular')
       $scope.openLangModal = function(word, trans){
         $('#changeLangModal').modal({});
         $scope.langModal = true;
+        $scope.changeLangErrorMsg = false;
         $scope.langModal = {
           model: {
             word: word,
@@ -353,16 +354,17 @@ angular.module('famousAngular')
       };
 
       // move to different lang over modal changeLang
-      $scope.moveToLang = function(lang_id){
-        $scope.langModal.model.trans.language_id = lang_id;
-        words.update($scope.langModal.model.trans, function (success) {
-          //$scope.words[$scope.words.indexOf(word)] = success;
-          if (lang_id !== $scope.lang.from.id) {
-            $scope.langModal.model.word.translations.splice($scope.langModal.model.word.translations.indexOf($scope.langModal.model.trans), 1);
-          }
-        });
-
-        $('#changeLangModal').modal('hide');
+      $scope.moveTranslationToLang = function(lang_id){
+        if (lang_id !== $scope.lang.to.id) {
+          $scope.langModal.model.trans.language_id = lang_id;
+          words.update($scope.langModal.model.trans, function (success) {
+            //$scope.words[$scope.words.indexOf(word)] = success;
+              $scope.langModal.model.word.translations.splice($scope.langModal.model.word.translations.indexOf($scope.langModal.model.trans), 1);
+          });
+          $('#changeLangModal').modal('hide');
+        } else {
+          $scope.changeLangErrorMsg = true;
+        }
       };
 
     }])
