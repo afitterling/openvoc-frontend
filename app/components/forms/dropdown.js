fd00e22537b68e37f83aa2a106c2b348809be0be
Alexander Fitterling <info@sp33c.de>
2015-01-25 13:07:53 +0100

    fixed dropdown

--- app/components/forms/dropdown.js
+++ app/components/forms/dropdown.js
@@ -2,13 +2,13 @@
 
 angular.module('directives.formHelpers', [])
 
-  .directive('dropdown', ['ValidationActionsStore','$log', function (ValidationActionsStore, $log) {
+  .directive('dropdown', ['ValidationActionsStore', '$log', function (ValidationActionsStore, $log) {
     return {
       restrict: 'E',
       templateUrl: 'components/forms/dropdown.html',
       scope: {
         listModel: '=',
-        selected : '=',
+        selected: '=',
         model: '=',
         name: '@',
         onChange: '&',
@@ -16,45 +16,67 @@ angular.module('directives.formHelpers', [])
       },
       link: function (scope, elm, attrs) {
 
-        // do a simple watch for setting the current value to other triggered changes
-        scope.$watch('model', function(newVal) {
-          scope.select(newVal.id);
-          spliceMenuItems();
-          scope.current = newVal;
-          scope.model = scope.current;
-          scope.ready = true;
-        });
+        var id = null;
 
-        var spliceMenuItems = function () {
-          scope.menuItems = [];
-          angular.copy(scope.listModel, scope.menuItems);
-          scope.menuItems.splice(scope.listModel.indexOf(scope.current), 1);
-        };
+        var promises = {};
+
+        if (angular.isDefined(attrs.id)) {
+          id = attrs.id;
+        }
+
+        ////////////// helpers ////////////////////////////////////
+
 
         scope.select = function (item_id) {
           angular.forEach(scope.listModel, function (item) {
-            if (angular.equals(item_id, item.id)) {
-              scope.current = item;
+            if (item_id === item.id) {
+              scope.model = item;
               return;
             }
           });
 
-          scope.model = scope.current;
-          // why you need this: the model binding is two way to some upper scope model wait until it is really updated
-          var cancel = scope.$watch('model', function () {
-            scope.onChange();
-            cancel();
-          });
+          // obsolete!
 
-          ValidationActionsStore.updateState(scope.name, scope.current);
-          $log.debug(scope.name, ' anyValidation =', ValidationActionsStore.anyValidation(scope.name));
-          $log.debug(scope.name, ' anyValidationArray =', ValidationActionsStore.anyValidationArray(scope.name));
+          //scope.model = scope.current;
+          // why you need this: the model binding is two way to some upper scope model wait until it is really updated
+//          var cancel = scope.$watch('model', function () {
+//            scope.onChange();
+//            cancel();
+//          });
+
         };
 
-        // run default on link up
-        scope.select(scope.selected);
-//        spliceMenuItems();
-//        scope.model = scope.current;
+        var setup = function () {
+          promises.selected = scope.$watch('selected', function (newId, oldId) {
+            if (!newId) {
+              return;
+            }
+
+            scope.select(newId);
+            spliceMenuItems();
+            scope.ready = true;
+
+            // update value store
+            ValidationActionsStore.updateState(scope.name, scope.model);
+//          $log.debug(scope.name, ' anyValidation =', ValidationActionsStore.anyValidation(scope.name));
+//          $log.debug(scope.name, ' anyValidationArray =', ValidationActionsStore.anyValidationArray(scope.name));
+          });
+          scope.$watch('model', function (newVal, oldVal) {
+            if (!newVal || newVal === oldVal) return;
+            // changed value
+
+            //$log.debug(id, newVal, oldVal);
+          });
+        };
+
+        var spliceMenuItems = function () {
+          scope.menuItems = [];
+          angular.copy(scope.listModel, scope.menuItems);
+          scope.menuItems.splice(scope.listModel.indexOf(scope.model), 1);
+        };
+
+        setup();
+
       }
     };
   }])
